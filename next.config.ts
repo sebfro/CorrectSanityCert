6fb80aefb533b60e43bb45954deade53bdf8697c
sfroyen <sebastian17pepp@gmail.com>
2024-10-22 20:35:48 +0200

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
+/** type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
+    images: {
+        domains: ["cdn.sanity.io", "via.placeholder.com"]
+    }
   /* config options here */
 };
 
